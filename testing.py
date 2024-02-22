539bf43fe5c338bedbbea2534692f425dff3589f
naim <MominNaim11@gmail.com>
2024-02-21 20:28:32 -0600

    Fixed the error in the code

--- testing.py
+++ testing.py
@@ -16,6 +16,7 @@ def file_into_a_list(txt):
 
 skip = r'!@#$%^?&<>`~;:'
 seperator_1 = r'. +-=/'
+numbers = r'1234567890'
 
 
 
@@ -29,12 +30,18 @@ def file_into_a_list(txt):
         if result[x] in skip:
             continue
         elif result[x] in seperator_1:
-            if len(funnel_list) != 0:
-                new_word = ''.join(char for char in funnel_list)
-                main_list.append(new_word)
-                funnel_list = []
+            if result[x] == '.':
+                if result[x + 1] in numbers:
+                    funnel_list.append(result[x])
             else:
-                continue
+                if len(funnel_list) != 0:
+                    new_word = ''.join(char for char in funnel_list)
+                    main_list.append(new_word)
+                    funnel_list = []
+                else:
+                    continue
+
+                
 
         else:
             if x == len(result) - 1:
@@ -46,10 +53,10 @@ def file_into_a_list(txt):
                 funnel_list.append(result[x])
                 
 
+    lowercased_list = [item.lower() if isinstance(item, str) else item for item in main_list]
+    print(lowercased_list)
 
-    print(main_list)
 
-
-testing = '#hell.o an%d w3lco+me t^e>s<t&i!n?g'
+testing = 'tree, 12.34 Blue. 56.78 hot. 91011 rock 12.13 star 91011 Moon. 91.012'
 
 file_into_a_list(testing)
